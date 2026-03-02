ce50cd3a5f7950e8e6756226a463d8d921acb4e8
wimos-ai <willjmos@iastate.edu>
2026-03-02 16:38:10 -0600

    Got lassert building

--- src/lassert.h
+++ src/lassert.h
@@ -1,21 +1,12 @@
 #ifndef _LASSERT_H_
 #define _LASSERT_H_
 
-#ifdef __cplusplus
-extern "C"
-{
-#endif
-
-    void lassert_fail(const char *expr, const char *file, int line);
+void lassert_fail(const char *expr, const char *file, int line);
 
 #ifndef LASSERT
 #define LASSERT(expr) \
     if (!(expr))      \
     lassert_fail(#expr, __FILE__, __LINE__)
 #endif // !LASSERT
-
-#ifdef __cplusplus
-}
-#endif
 
 #endif // !_LASSERT_H_
