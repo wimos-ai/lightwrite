2a05be6ea49a63867f1b856332cf7780a96d69a4
wimos-ai <willjmos@iastate.edu>
2026-03-02 16:36:51 -0600

    Got font building cpp

--- src/font.h
+++ src/font.h
@@ -1,30 +1,21 @@
 #ifndef FONT_H
 #define FONT_H
 
-#ifdef __cplusplus
-extern "C"
-{
-#endif
-
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_ttf.h>
 
-	typedef struct _Font_Data_
-	{
-		const char *text;
+typedef struct _Font_Data_
+{
+	const char *text;
 
-		SDL_Texture *texture;
-		SDL_Rect rect;
+	SDL_Texture *texture;
+	SDL_Rect rect;
 
-		int font_w;
-		int font_h;
-	} Font_Data;
+	int font_w;
+	int font_h;
+} Font_Data;
 
-	Font_Data prepare_string(TTF_Font *font, SDL_Renderer *renderer, int x, int y,
-							 const char *string, SDL_Color color);
-
-#ifdef __cplusplus
-}
-#endif
+Font_Data prepare_string(TTF_Font *font, SDL_Renderer *renderer, int x, int y,
+						 const char *string, SDL_Color color);
 
 #endif // !FONT_H
