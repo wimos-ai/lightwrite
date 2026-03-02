4d747a6e43837009556abf85d80a03b5f82740a5
wimos-ai <willjmos@iastate.edu>
2026-03-02 16:38:46 -0600

    Got logger building

--- src/logger.h
+++ src/logger.h
@@ -1,28 +1,23 @@
 #ifndef _LOGGER_H_
 #define _LOGGER_H_
 
-#ifdef __cplusplus
-extern "C"
-{
-#endif
-
 #include <stdbool.h>
 
-    typedef enum _Log_Level_
-    {
-        LOG_LEVEL_FATAL,
-        LOG_LEVEL_ERROR,
-        LOG_LEVEL_WARN,
-        LOG_LEVEL_DEBUG,
-        LOG_LEVEL_INFO,
+typedef enum _Log_Level_
+{
+    LOG_LEVEL_FATAL,
+    LOG_LEVEL_ERROR,
+    LOG_LEVEL_WARN,
+    LOG_LEVEL_DEBUG,
+    LOG_LEVEL_INFO,
 
-        LOG_LEVEL_MAX_TAG
-    } Log_Level;
+    LOG_LEVEL_MAX_TAG
+} Log_Level;
 
-    bool logger_init(void);
-    void logger_destroy(void);
+bool logger_init(void);
+void logger_destroy(void);
 
-    void logger_print(Log_Level level, const char *message, ...);
+void logger_print(Log_Level level, const char *message, ...);
 
 #ifndef LOG_FATAL
 #define LOG_FATAL(...) logger_print(LOG_LEVEL_FATAL, __VA_ARGS__)
@@ -47,9 +42,5 @@ extern "C"
 #ifndef LOG_INFO
 #define LOG_INFO(...) logger_print(LOG_LEVEL_INFO, __VA_ARGS__)
 #endif // !LOG_INFO
-
-#ifdef __cplusplus
-}
-#endif
 
 #endif // !_LOGGER_H_
