823dba9550c3e93b7882aa0a0afbedf2a317e104
wimos-ai <willjmos@iastate.edu>
2026-03-07 20:53:37 -0600

    Modernized keybinds

---
+++ include/keybinds.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+void keybinds_on_up(int keycode);
+void keybinds_on_down(int keycode);
+bool keybinds_is_down(int keycode);
--- src/keybinds.cpp
+++ src/keybinds.cpp
@@ -1,43 +1,48 @@
-#include "keybinds.h"
+#include "keybinds.hpp"
 #include "logger.h"
 
 #include <SDL2/SDL_keycode.h>
+#include <array>
 
-static Keybind keybinds[] = {
-    {SDLK_LCTRL, false},
-    {SDLK_LSHIFT, false}};
-static const unsigned int keybinds_len = sizeof(keybinds) / sizeof(keybinds[0]);
+struct Keybind
+{
+    int keycode;
+    bool pressed;
+};
 
+static std::array keybinds{
+    Keybind{SDLK_LCTRL, false},
+    Keybind{SDLK_LSHIFT, false}};
 
 void keybinds_on_up(int keycode)
 {
-    for (unsigned int i = 0; i < keybinds_len; ++i)
+    for (auto &keybind : keybinds)
     {
-        if (keybinds[i].keycode == keycode)
+        if (keybind.keycode == keycode)
         {
-            keybinds[i].pressed = false;
+            keybind.pressed = false;
         }
     }
 }
 
 void keybinds_on_down(int keycode)
 {
-    for (unsigned int i = 0; i < keybinds_len; ++i)
+    for (auto &keybind : keybinds)
     {
-        if (keybinds[i].keycode == keycode)
+        if (keybind.keycode == keycode)
         {
-            keybinds[i].pressed = true;
+            keybind.pressed = true;
         }
     }
 }
 
 bool keybinds_is_down(int keycode)
 {
-    for (unsigned int i = 0; i < keybinds_len; ++i)
+    for (auto &keybind : keybinds)
     {
-        if (keybinds[i].keycode == keycode)
+        if (keybind.keycode == keycode)
         {
-            return keybinds[i].pressed;
+            return keybind.pressed;
         }
     }
     return false;
